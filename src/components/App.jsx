f6f2eea2ab94a870c4e8603475e8fcace5189cb7
Illia12august <121337310+Illia12august@users.noreply.github.com>
2023-09-18 22:44:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,57 +44,6 @@ export const App = () => {
         contact.name.toLocaleLowerCase() === newName.toLocaleLowerCase()
     );
   };
-  // state = {
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
-  // componentDidMount() {
-  //   const savedContacts = localStorage.getItem('contacts');
-  //   if (savedContacts !== null) {
-  //     const contacts = JSON.parse(savedContacts);
-  //     this.setState({ contacts });
-  //   }
-  // }
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (prevState.contacts !== this.state.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // }
-  // handleAddBook = newContact => {
-  //   if (this.handleExists(newContact.name)) {
-  //     alert('Contact with this name already exists!');
-  //     return;
-  //   }
-  //   this.setState(prevState => {
-  //     return {
-  //       contacts: [...prevState.contacts, newContact],
-  //     };
-  //   });
-  // };
-
-  // handleFilterChange = newFilter => {
-  //   this.setState({ filter: newFilter });
-  // };
-
-  // onDelete = delId => {
-  //   this.setState(prevState => {
-  //     return {
-  //       contacts: prevState.contacts.filter(contact => contact.id !== delId),
-  //     };
-  //   });
-  // };
-
-  // handleExists = newName => {
-  //   return this.state.contacts.some(
-  //     contact =>
-  //       contact.name.toLocaleLowerCase() === newName.toLocaleLowerCase()
-  //   );
-  // };
   const filteredContactList = contacts.filter(contact => {
     return contact.name.toLowerCase().includes(filter);
   });
